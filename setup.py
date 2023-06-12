747ddb16af7b0a88619a1aa8c707eeec4577465a
zhenkaivip <zhenkaivip@163.com>
2023-06-12 10:04:35 +0800

    a2

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ from setuptools import find_packages, setup
 DESCRIPTION = 'Open source Python library converting pdf to docx.'
 EXCLUDE_FROM_PACKAGES = ["build", "dist", "test"]
 
+
 # read version number from version.txt, otherwise alpha version
 # Github CI can create version.txt dynamically.
 def get_version(fname):
@@ -14,9 +15,10 @@ def get_version(fname):
         with open(fname, "r", encoding="utf-8") as f:
             version = f.readline().strip()
     else:
-        version = '0.5.6a1'
+        version = '0.5.6a2'
 
     return version
+
 
 # Load README.md for long description
 def load_long_description(fname):
@@ -28,10 +30,11 @@ def load_long_description(fname):
 
     return long_description
 
+
 def load_requirements(fname):
     try:
         # pip >= 10.0
-        from pip._internal.req import parse_requirements        
+        from pip._internal.req import parse_requirements
     except ImportError:
         # pip < 10.0
         from pip.req import parse_requirements
@@ -44,24 +47,25 @@ def load_requirements(fname):
 
     return requirements
 
+
 setup(
-    name="pdf2docx",    
+    name="pdf2docx",
     version=get_version("version.txt"),
     keywords=["pdf-to-word", "pdf-to-docx"],
     description=DESCRIPTION,
     long_description=load_long_description("README.md"),
     long_description_content_type="text/markdown",
-    license="GPL v3", 
+    license="GPL v3",
     author="dothinking",
     author_email="train8808@gmail.com",
     url="https://github.com/dothinking/pdf2docx",
     packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
-    include_package_data=True,    
+    include_package_data=True,
     zip_safe=False,
     install_requires=load_requirements("requirements.txt"),
     python_requires=">=3.6",
     entry_points={
         "console_scripts": [
             "pdf2docx=pdf2docx.main:main"
-            ]},
+        ]},
 )
